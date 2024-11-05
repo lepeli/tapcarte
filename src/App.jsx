a7a9877da4cbbdb5e510635211f4210bd026a100
lepeli <accounts@yvan.dev>
2024-11-05 09:45:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import { Navbar } from './navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+import logo from "../assets/logo.png"
+
+
+export const Navbar = () => {
+    return <div className="navbar">
+        <img src={logo} alt="logo"/>
+    </div>
+}
