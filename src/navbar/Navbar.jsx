cc102e48028ccfba44b56e0f13f74ca467c1227c
Wagoat <wagowag66@gmail.com>
2024-11-06 14:59:42 +0100

    Modified the navbar to change the color and add the hotel_logo

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 
+import hotel_logo from "../assets/image_5.png"
 import logo from "../assets/tapcartelogo.png"
 import "./Navbar.css"
 
 export const Navbar = () => {
     return <div className="navbar">
-        <img src={logo} alt="logo"/>
+        <img src={logo} alt="logo" className="tap_logo"/>
+        <img src={hotel_logo} alt="hotel_logo" className="hotel_logo"/>
     </div>
 }
